f9fe72dd0feb961d08f8f794654f3984a74a753b
jeremiahsteggall <58449670+jeremiahsteggall@users.noreply.github.com>
2020-08-11 13:13:01 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,96 @@
+package com.mass.liferacer;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
+
+import com.mass.liferacer.graphics.Screen;
+
+public class Main extends Canvas implements Runnable{
+	private static final long serialVersionUID = 1L;
+	
+	public int width = 300;
+	public int height = width / 16 * 9;
+	public int scale = 3;
+	
+	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
+	
+	private Screen screen;
+	
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+	
+	public Main() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		screen = new Screen(width, height);
+		
+		frame = new JFrame();
+	}
+	
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public  synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		while (running) {
+			update();
+			render();
+		}
+	}
+	
+	public void update() {
+	}
+	
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		screen.clear();
+		screen.render();
+		
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		g.dispose();
+		bs.show();
+	}
+	
+	public static void main (String[] args) {
+		Main main = new Main();
+		main.frame.setResizable(false);
+		main.frame.setTitle("Life Racer pre-alpha 0.0.1");
+		main.frame.add(main);
+		main.frame.pack();
+		main.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		main.frame.setLocationRelativeTo(null);
+		main.frame.setVisible(true);
+		
+		main.start();
+	}
+}
